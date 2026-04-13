4b6f849e2cb7d2d69b6684e74aaa45610b89decb
Takeru Tokoro <takerutkr0130@gmail.com>
2026-04-14 02:54:59 +0900

    Initial commit

---
+++ test/corekeeper-server-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CorekeeperServerCdk from '../lib/corekeeper-server-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/corekeeper-server-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CorekeeperServerCdk.CorekeeperServerCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
